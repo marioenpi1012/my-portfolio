d281f2bf4a9359583688cd1a8f3e4bb198362b34
Mario <mariopineda179@gmail.com>
2021-02-18 23:07:08 -0600

    form contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,22 @@
 import {Helmet } from 'react-helmet'
+import emailjs from 'emailjs-com'
+
 const Contact = () => {
+    function validation(){
+
+    }
+    function sendEmail(e) {
+    e.preventDefault();
+    
+    emailjs.sendForm('service_2qiqeln', 'template_gwccvxo', e.target, 'user_I1AkK85L0I5ZLSeHrIYD6')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset()
+    } 
+
     return (
         <div>
             <Helmet>
@@ -8,10 +25,11 @@ const Contact = () => {
             
             <div className="contactForm">
                 <h1 id='formTitle'>Contact Me</h1>
-                <form action="" id='form'>
-                    <input type="text" placeholder='Your Name' name='name'/>
-                    <input type="text" placeholder='Your Email' name='email'/>
-                    <textarea type="text" placeholder='Message' name='message'/>
+                <form onSubmit={sendEmail} id='form' onChange={validation}>
+                    <input type="text" placeholder='Your Name' name='name' required/>
+                    <input type="text" placeholder='Your Email' name='email' required/>
+                    <textarea type="text" placeholder='Message' name='message' required/>
+                    <input type="submit" className='btn'/>
                 </form>
             </div>
         </div>
