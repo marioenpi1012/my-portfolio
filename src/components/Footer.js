721a710ed89ee38e206884a122ca14355301244a
Mario <mariopineda179@gmail.com>
2021-02-27 19:46:57 -0600

    Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import {FaLinkedin} from "react-icons/fa"
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <div className="footer-line"></div>
+            <ul>
+                <li> <a href="https://www.linkedin.com/in/mario-pineda-9a6b68186/"><FaLinkedin /></a></li>
+            </ul>
+            
+        </div>
+    )
+}
+
+export default Footer
